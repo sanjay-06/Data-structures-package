0225e812d362d602b7fc2d3c16e4f98f11f7eedd
sanjay-06 <59244273+sanjay-06@users.noreply.github.com>
2020-05-17 10:57:18 +0530

    Add files via upload

---
+++ queue.h
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<string>
+using namespace std;
+template<typename T>
+class queue
+{
+	int front;
+	int rear;
+	T *q;
+	int size;
+	public:
+		queue();
+		queue(T);
+		void enqueue(T);
+		T dequeue();
+};
+template<class T>
+queue<T>::queue()
+{
+	front=-1;
+	rear=-1;
+	size=10;
+	q=new T[size]();
+}
+template<class T>
+queue<T>::queue(T n)
+{
+	front=-1;
+	rear=-1;
+	size=n;
+	q=new T[size]();
+}
+template<class T>
+void queue<T>::enqueue(T d)
+{
+	if(rear==size)
+	cout<<"queue is full";
+	else
+	{
+		rear++;
+		q[rear]=d;
+	}
+}
+template<class T>
+T queue<T>::dequeue()
+{
+	T element;
+	if(rear==front)
+	cout<<"queue is empty";
+	else
+	{
+		front++;
+		element=q[front];
+	}
+	return element;
+}
